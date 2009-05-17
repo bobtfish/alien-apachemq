89fe7d98902033c9f0ef6d5eb62774e1e5ca70f8
t0m <bobtfish@bobtfish.net>
2009-05-17 16:33:46 +0100

    Add rest of docs needed before this goes to CPAN

--- lib/Alien/ActiveMQ.pm
+++ lib/Alien/ActiveMQ.pm
@@ -9,17 +9,25 @@ use namespace::autoclean;
 
 my $VERSION = 0.00001;
 
-method get_installed_versions {}
+#method get_installed_versions {}
 
-method get_version_dir ($version) {}
-
-method is_version_installed ($version) {}
-
-method run_server ($version) {
+method get_version_dir ($version) {
     if (!$version) {
         #$version = ( $self->get_installed_versions )[0];
+        $version = '5.2.0';
     }
-    my $dir = dir( dist_dir('Alien-ActiveMQ'), '5.2.0' );
+    return dir( dist_dir('Alien-ActiveMQ'), $version );
+}
+
+#method is_version_installed ($version) {}
+
+method get_licence_filename ($version) {
+    my $dir = $self->get_version_dir($version);
+    return file($dir, 'LICENSE');
+}
+
+method run_server ($version) {
+    my $dir = $self->get_version_dir($version);
     my @cmd = (file( $dir, 'bin', 'activemq' ));
 
     # TODO - might be nice to grab the output of the server and wait til
@@ -28,7 +36,10 @@ method run_server ($version) {
     my $h = start \@cmd, \undef;
     sleep 20;
 
-    return Scope::Guard->new(sub { warn("Killing ApacheMQ..."); $h->signal ( "KILL" ) });
+    return Scope::Guard->new(sub { 
+        warn("Killing ApacheMQ...");
+        $h->signal ( "KILL" ) 
+    });
 }
 
 1;
@@ -49,7 +60,13 @@ Alien::ActiveMQ - Manages installs of versions of Apache ActiveMQ, and provides 
         # Apache MQ is now running on the default port, you
         # can now test your Net::Stomp based code
     }
-    # And Apache MQ shuts down once $mq goes out of scope here.
+    # And Apache MQ shuts down once $mq goes out of scope here
+
+=head1 DESCRIPTION
+
+This module, along with the bundled C< install-apachemq > script,
+helps to manage installations of the Apache ActiveMQ message queueing software,
+from L<http://activemq.apache.org>.
 
 =head1 FUNCTIONS
 
@@ -59,6 +76,52 @@ Runs an ActiveMQ server instance for you.
 
 Returns a value which you must keep in scope until you want the ActiveMQ server
 to shutdown.
+
+=head2 get_version_dir ([ $version ])
+
+Returns a L<Path::Class::Dir> object to where a particular version of ActiveMQ
+is installed.
+
+=head2 get_license_file ([ $version ])
+
+Returns a L<Path::Class::File> object representing the text file containing the
+license for a particular version of Apache ActiveMQ.
+
+=head1 TODO
+
+This is the first release of this code, and as such, it is very light on
+features, and probably full of bugs.
+
+Please see comments in the code for features planned and changes needed.
+
+Patches (or forks on github) are, as always, welcome.
+
+=head1 LINKS
+
+=over
+
+=item L<http://activemq.apache.org/> - Apache ActiveMQ project homepage.
+
+=item L<Net::STOMP> - Interface to the Streamed Text Oriented Message Protocol in perl.
+
+=item L<Catalyst::Engine::STOMP> - Use the power of Catalyst dispatch to route job requests.
+
+=back
+
+=head1 AUTHORS
+
+    Tomas Doran (t0m) <bobtfish@bobtfish.net>
+    Zac Stevens (zts) <zts@cryptocracy.com>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2009 by Tomas Doran.
+
+This is free software; you can redistribute it and/or modify it under the same
+terms as perl itself.
+
+Note that the Apache MQ code which is installed by this software is licensed
+under the Apache 2.0 license, which is included in the installed software.
 
 =cut
 
